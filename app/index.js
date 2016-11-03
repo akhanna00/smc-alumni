adc5fe17519649f5c365a8805a509a19325cb9fa
Arun <arunkhanna00@gmail.com>
2016-11-02 17:08:46 -0700

    add angularjs routing

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-console.log("HI");
+// Create Angular Module
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+// Configure routes
+myApp.config(["$routeProvider", function($routeProvider) {
+	$routeProvider
+
+	.when('/', {
+		templateUrl : 'pages/home.min.html'
+	})
+
+	.when('/about', {
+		templateUrl : 'pages/about.min.html'
+	})
+
+	.when('/contact', {
+		templateUrl : 'pages/contact.min.html'
+	})
+
+	.when('/join', {
+		templateUrl : 'pages/join.min.html'
+	})
+
+	.when('/support', {
+		templateUrl : 'pages/support.min.html'
+	})
+	
+}]);
