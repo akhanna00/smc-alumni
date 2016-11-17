9bbb21c465df85a8d783cac40f36a000d80c8934
Arun <arunkhanna00@gmail.com>
2016-11-16 16:40:37 -0800

    add additional controller

--- app/index.js
+++ app/index.js
@@ -12,7 +12,8 @@ myApp.config(["$routeProvider", function($routeProvider) {
 	})
 
 	.when('/events', {
-		templateUrl : 'pages/events.min.html'
+		templateUrl : 'pages/events.min.html',
+		controller : 'eventController'
 	})
 
 	.when('/contact', {
@@ -38,27 +39,143 @@ myApp.config(["$routeProvider", function($routeProvider) {
 }]);
 
 // Make a controller for the home page
-myApp.controller('homeController', ['$scope', function($scope) {
+myApp.controller('eventController', ['$scope', function($scope) {
 	// Store the JSON of the data to be displayed as events
 	$scope.events = events;
 }]);
 
+// Make a controller for the home page
+myApp.controller('homeController', ['$scope', function($scope) {
+	// Store the JSON of the data to be displayed as events
+	$scope.events = events;
+	$scope.stories = stories;
+}]);
+
 // Data to be stored
+// Events
 var events = [
 	{
 		name: 'Homecoming',
-		time: "November 5th",
+		date: '11/16/2016',
+		time: '5:00 PM',
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
 	},
 	{
-		name: 'International Day', 
-		time: 'November 6th',
+		name: 'Homecoming',
+		date: '11/18/2016',
+		time: '7:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/15/2016',
+		time: '4:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},{
+		name: 'Homecoming',
+		date: '11/16/2016',
+		time: '5:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/18/2016',
+		time: '7:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/15/2016',
+		time: '4:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	}, 
+	{
+		name: 'Homecoming',
+		date: '11/16/2016',
+		time: '5:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/18/2016',
+		time: '7:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/15/2016',
+		time: '4:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	}, 
+	{
+		name: 'Homecoming',
+		date: '11/16/2016',
+		time: '5:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/18/2016',
+		time: '7:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/15/2016',
+		time: '4:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	}, 
+	{
+		name: 'Homecoming',
+		date: '11/16/2016',
+		time: '5:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/18/2016',
+		time: '7:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/15/2016',
+		time: '4:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	}, 
+	{
+		name: 'Homecoming',
+		date: '11/16/2016',
+		time: '5:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/18/2016',
+		time: '7:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Homecoming',
+		date: '11/15/2016',
+		time: '4:00 PM',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	}
+];
+
+// Alumni Stories
+var stories = [
+	{
+		name: 'Lorem ipsum',
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
+	},
+	{
+		name: 'Lorem ipsum', 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
 
 	}, 
 	{
-		name: "Veteran's Day",
-		time: 'November 8th',
+		name: "Lorem ipsum",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit sit amet condimentum mollis. Mauris commodo malesuada urna, pellentesque eleifend"
 
 	}
